86d67c87ca11e51944041c26633da11a989aa93c
Xon <635541+Xon@users.noreply.github.com>
2026-03-31 15:00:07 +0800

    3rd party add-on compatibility fix where the slow query logged is injected after the XF entity manager is setup; this causes transaction handling to break causing impossible bugs

--- upload/src/addons/SV/SlowQueryLogger/Listener.php
+++ upload/src/addons/SV/SlowQueryLogger/Listener.php
@@ -7,6 +7,7 @@ use SV\SlowQueryLogger\Db\Mysqli\SlowQueryLogAdapter\FakeParent;
 use XF\App;
 use function class_alias;
 use function class_exists;
+use function property_exists;
 
 abstract class Listener
 {
@@ -26,7 +27,8 @@ abstract class Listener
         // just in case
         if ($result)
         {
-            $app->container()->set('db', function ($c) use ($app) {
+            $c = $app->container();
+            $c->set('db', function ($c) use ($app) {
                 $config = $c['config'];
 
                 $dbConfig = $config['db'];
@@ -42,10 +44,36 @@ abstract class Listener
 
                 return $db;
             });
+
+            $keys = [
+                'permission.cache',
+                // need to patch the \XF\Mvc\Entity\Manager::$db is something has touched \XF::em() before this code runs
+                // otherwise the transaction state can get out of sync and then causes really *weird* bugs
+                'em',
+                // todo; check other built-ins that may require patching
+            ];
+
+            foreach ($keys as $key)
+            {
+                if ($c->isCached($key))
+                {
+                    self::patchDbProperty($c[$key]);
+                }
+            }
         }
         else
         {
             \XF::logException(new \Exception('Unable to alias existing adapter class for slow query logging!'));
         }
+    }
+
+    protected static function patchDbProperty(object $obj): void
+    {
+        \Closure::bind(function () {
+            if (property_exists($this, 'db'))
+            {
+                $this->db = \XF::db();
+            }
+        }, $obj, $obj)();
     }
 }
