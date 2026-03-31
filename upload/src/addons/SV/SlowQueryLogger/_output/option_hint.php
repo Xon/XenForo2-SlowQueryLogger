dc2f2fb7799af81663d2a4892f91070192ee685e
Xon <635541+Xon@users.noreply.github.com>
2026-04-01 03:13:46 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/SlowQueryLogger/_output/option_hint.php
@@ -0,0 +1,22 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property float|null $sv_slowquery_threshold Slow query threshold
+ * @property float|null $sv_slowtransaction_threshold Slow transaction threshold
+ * @property non-negative-int|null $sv_toomany_queries Too many queries threshold
+ * @property bool|null $sv_toomany_queries_public_only Only log too many queries for front-end
+ * @property bool|null $sv_toomany_queries_skip_alerts Skip alerting when checking for too many queries
+ */
+class Options
+{
+}
